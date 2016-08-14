5baf629cf48b64e2aa7e25eb1ab688ca520f32ee
brandonleichty <brandonl@me.com>
2016-08-14 10:56:25 -0500

    More minor adjustments

--- js/app.js
+++ js/app.js
@@ -2,23 +2,21 @@
 
 //---GLOBAL VARIABLES---//
 
-// search field variable to append to html
-var $studentSearch = $('<div class="student-search"><input placeholder="Search for students..."><button>Search</button></div>');
-
+// assings the number of students to numberOfStudents variable
+var numberOfStudents = $(".student-item").length;
 
 // append student search field to header
-$(".page-header").append($studentSearch);
+$(".page-header").append('<div class="student-search"><input placeholder="Search for students..."><button>Search</button></div>');
 
-// display only the first ten students
+// display only the first ten students *is "display: none" better?*
 $(".student-item.cf:nth-of-type(n+11)").hide();
 
 
+// append pagination class and unordered list to end of body
+$("body").append('<div class="pagination"><ul></ul></div>');
 
-
-
-
-/*
-{
-    display: none;
+var pageNum = 1;
+for(i = 0; i < numberOfStudents; i += 10){
+  $(".pagination").append('<li><a href="#">' + pageNum + '</a></li>');
+  pageNum += 1;
 }
-*/
