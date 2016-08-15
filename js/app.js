520832df7e41511eee39e33ab1d114c941bb6d27
brandonleichty <brandonl@me.com>
2016-08-15 14:09:11 -0500

    Added to pagination code
    Now when the user clicks on a page number, the appropriate amount of
    students will be displayed.

--- js/app.js
+++ js/app.js
@@ -16,17 +16,26 @@ $(".student-item.cf:nth-of-type(n+11)").hide();
 $("body").append('<div class="pagination"><ul></ul></div>');
 
 var pageNum = 1;
-for(i = 0; i < numberOfStudents; i += 10){
+for(i = 10; i < numberOfStudents; i += 10){
   $(".pagination").append('<li><a href="#">' + pageNum + '</a></li>');
   pageNum += 1;
 }
-
+var currentPage = 1;
 // set the first page/link to active
 $(".pagination a").first().addClass("active");
 
 $(".pagination a").click(function(){
     $("a").removeClass("active");
     $(this).addClass("active");
+    currentPage = parseInt($(this).text()); //gets the current page and converts it to an integer
+    var startSlice = currentPage * 10;
+    var endSlice = (currentPage) * 10 + 10;
+    if(currentPage === 1){
+      startSlice = currentPage - 1;
+      endSlice = currentPage + 9;
+    }
+    console.log("startSlice is equal to: " + startSlice);
+    console.log("endSlice is equal to: " + endSlice);
     $(".student-item").hide();
-    $(".student-item.cf:lt(9)").show(); //figure out a way to show ten items at a time based upon button pressed
+    $(".student-item.cf").slice(startSlice,endSlice).show(); //figure out a way to show ten items at a time based upon button pressed
 })
