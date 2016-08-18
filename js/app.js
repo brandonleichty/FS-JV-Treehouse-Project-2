6e573397d27217bda275d5a42766ce0da7a33fb6
brandonleichty <brandonl@me.com>
2016-08-17 20:34:29 -0500

    More tweaking

--- js/app.js
+++ js/app.js
@@ -9,20 +9,27 @@ var numberOfStudents = $(".student-item.cf").length;
 $(".page-header").append('<div class="student-search"><input placeholder="Search for students..."><button>Search</button></div>');
 
 // display only the first ten students *is "display: none" better?*
-$(".student-item.cf:nth-of-type(n+11)").hide();
+//$(".student-item.cf:nth-of-type(n+11)").hide();
 
-
+//$(".student-item.cf:gt(10)").hide();
 // append pagination class and unordered list to end of body
 $("body").append('<div class="pagination"><ul></ul></div>');
 
+
 function paginateFunction(){
+$(".student-item.cf:gt(9)").hide(); //shows only ten students
+
+
+  $(".pagination li").each(function(){ //clear pagination
+    $(this).remove();
+  })
 
   var pageNum = 1;
-
-  for(i = 10; i < numberOfStudents; i += 10){
-  $(".pagination").append('<li><a href="#" class="pageNumClass">' + pageNum + '</a></li>');
+  for(i = 10; i < numberOfStudents + 10; i += 10){
+  $(".pagination ul").append('<li><a href="#">' + pageNum + '</a></li>');
   pageNum += 1;
 }
+$(".pagination a").first().addClass("active");
 console.log("PAGINATION FUNCTION run");
 console.log("PageNum: " + pageNum);
 }
@@ -30,22 +37,23 @@ console.log("PageNum: " + pageNum);
 paginateFunction();
 
 
-var currentPage = 1;
-// set the first page/link to active
-$(".pagination a").first().addClass("active");
 
+// set the first page/link to active
+
+//var currentPage = 1;
 $(".pagination a").click(function(){
-    $("a").removeClass("active");
+  console.log("Number of students to show: " + $(".student-item.cf").length);
+    $(".pagination a").removeClass("active");
     $(this).addClass("active");
-    currentPage = parseInt($(this).text()); //gets the current page and converts it to an integer
+    var currentPage = parseInt($(this).text()); //gets the current page and converts it to an integer
     var startSlice = currentPage * 10;
     var endSlice = (currentPage) * 10 + 10;
     if(currentPage === 1){
       startSlice = currentPage - 1;
       endSlice = currentPage + 9;
     }
-    //console.log("startSlice is equal to: " + startSlice);
-    //console.log("endSlice is equal to: " + endSlice);
+    console.log("startSlice is equal to: " + startSlice);
+    console.log("endSlice is equal to: " + endSlice);
     $(".student-item").hide();
     $(".student-item.cf").slice(startSlice,endSlice).show(); //figure out a way to show ten items at a time based upon button pressed
 })
@@ -71,6 +79,6 @@ $(".student-search input").keyup(function(){
   numberOfStudents = $(".student-item.cf").length;
   console.log("The number of displible students are :" + numberOfStudents);
   paginateFunction();
-  $(".student-item.cf:nth-of-type(n+11)").hide();
+
 
 })
