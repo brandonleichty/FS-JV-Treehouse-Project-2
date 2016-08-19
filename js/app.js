990ad02e6200ad7442d2d7f2581f1796ce9ff4d6
brandonleichty <brandonl@me.com>
2016-08-18 21:39:50 -0500

    More adjusments
    Fixed issue with spaces in search string.
    Added more comments.

--- js/app.js
+++ js/app.js
@@ -1,13 +1,16 @@
-//JS file
-
 //---GLOBAL VARIABLES---//
 
 // assings the number of students to numberOfStudents variable
 var numberOfStudents = $(".student-item.cf").length;
 
+var studentDetailsString = [];
+
+//---PAGE APPENDS ON LOAD---//
+
 // append student search field to header
 $(".page-header").append('<div class="student-search"><input placeholder="Search for students..."><button>Search</button></div>');
 
+// append "No students found!" paragraph and emmidately hide (will be shown if there are no matching search results)
 $(".page-header").append('<div class="noStudentFound"><p>No students found! </p></div>');
 $(".noStudentFound").hide();
 
@@ -16,35 +19,31 @@ $("body").append('<div class="pagination"><ul></ul></div>');
 
 paginateFunction();
 
+
+
+//---FUNCTIONS---//
+
 function paginateFunction(){
-$(".student-item.cf:gt(9)").hide(); //shows only ten students
+    $(".student-item.cf:gt(9)").hide(); //shows only ten students
 
-  $(".pagination li").each(function(){ //clear pagination
-    $(this).remove();
-  })
+    //clear pagination
+    $(".pagination li").each(function(){
+          $(this).remove();
+      })
 
-  var pageNum = 1; //resets pageNum to 1 before entering loop
-  //if(numberOfStudents >= 11){
-      //$(".pagination ul").append('<li><a href="#">' + pageNum + '</a></li>');
-      //pageNum += 1
+      var pageNum = 1; //resets pageNum to 1 before entering loop
+
+      //adds pagination based upon number of students to show
       for(i = 10; i <= Math.ceil(numberOfStudents); i += 10){
-      $(".pagination ul").append('<li><a href="#">' + pageNum + '</a></li>');
-      pageNum += 1;
-  }
-$(".pagination a").first().addClass("active");
-console.log("PAGINATION FUNCTION run");
-console.log("PageNum: " + pageNum);
+            $(".pagination ul").append('<li><a href="#">' + pageNum + '</a></li>');
+            pageNum += 1;
+        }
+      //sets page 1 to the active page
+     $(".pagination a").first().addClass("active");
 }
 
-
-
-
-//var currentPage = 1;
-$(".pagination").on("click", "a", pageClickFunction);
-
+//moves between each page showing the proper students upon each page click
 function pageClickFunction() {
-
-  console.log("Number of students to show: " + $(".student-item.cf").length);
 
     $("a").removeClass("active");
     $(this).addClass("active");
@@ -52,37 +51,46 @@ function pageClickFunction() {
     var currentPage = parseInt($(this).text()); //gets the current page and converts it to an integer
     var startSlice = currentPage * 10;
     var endSlice = (currentPage) * 10 + 10;
+
     if(currentPage === 1){
-      startSlice = currentPage - 1;
-      endSlice = currentPage + 9;
+        startSlice = currentPage - 1;
+        endSlice = currentPage + 9;
     }
-    console.log("startSlice is equal to: " + startSlice);
-    console.log("endSlice is equal to: " + endSlice);
     $(".student-item").hide();
     $(".student-item.cf").slice(startSlice,endSlice).show();
 }
 
+// searches students
+function searchFunction(){
 
-$(".student-search input").keyup(function(){
+    var $search = $(".student-search input").val().trim().toLowerCase().replace(/ /g,''); // gets user input from from search field, trims each end, changes to lowercaes, and removes spaces
 
-    var $search = $(".student-search input").val().trim().toLowerCase();
+    $(".student-details").each(function(){ // loops through each student
 
-        $(".student-item h3, .student-item .email").each(function(){
+        studentDetailsString = $(this).text().trim().toLowerCase().replace(/ /g,'');
 
-            if ($.trim($(this).text()).search(new RegExp($search, "i")) < 0) { //need to remove white space between first and last name!!!
-                  $(this).closest(".student-item").removeClass("cf").hide(); //$(this).closest(".student-item").hide() //move up the DOM tree from h3 to .student-item class
+        if (studentDetailsString.search(new RegExp($search, "i")) < 0) {
+                $(this).closest(".student-item").removeClass("cf").hide();
             } else {
-                  $(this).closest(".student-item").addClass("cf").show(); //$(this).closest(".student-item").show(); //move up the DOM tree from h3 to .student-item class
+                $(this).closest(".student-item").addClass("cf").show();
               }
         })
 
   numberOfStudents = $(".student-item.cf").length;
 
+  // checks to see if there are no search matches - dispalys "No students found!" if there are no results
   if(numberOfStudents === 0){
     $(".noStudentFound").show();
   } else {
     $(".noStudentFound").hide();
   }
-  //console.log("The number of displible students are :" + numberOfStudents);
+
+  // re-paginate with new search results after each keyup
   paginateFunction();
-})
+}
+
+//---EVENT LISTENERS---//
+
+$(".pagination").on("click", "a", pageClickFunction);
+
+$(".student-search input").keyup(searchFunction);
