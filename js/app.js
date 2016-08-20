e1a54f857572ce6a5d28f1a39088d31074032176
brandonleichty <brandonl@me.com>
2016-08-20 11:32:48 -0500

    Added more comments

--- js/app.js
+++ js/app.js
@@ -1,9 +1,15 @@
+//Pagination on page load:
+paginateFunction();
+
+
 //---GLOBAL VARIABLES---//
 
 // assings the number of students to numberOfStudents variable
 var numberOfStudents = $(".student-item.cf").length;
 
 var studentDetailsString = [];
+
+
 
 //---PAGE APPENDS ON LOAD---//
 
@@ -17,26 +23,24 @@ $(".noStudentFound").hide();
 // append pagination class and unordered list to end of body
 $("body").append('<div class="pagination"><ul></ul></div>');
 
-paginateFunction();
-
 
 
 //---FUNCTIONS---//
 
 function paginateFunction(){
-
+    console.log("paginateFunction!!!");
 
     $(".student-item.cf:gt(9)").hide(); //shows only ten students
 
     //clear pagination
     $(".pagination li").each(function(){
           $(this).remove();
-      })
+      });
 
       var pageNum = 1; //resets pageNum to 1 before entering loop
 
       //adds pagination based upon number of students to show
-      if (numberOfStudents > 10) {
+      if (numberOfStudents > 10) { //doesn't display pagination if there aren't enough students
       for(i = 10; i <= (Math.ceil(numberOfStudents / 10)) * 10; i += 10){
             $(".pagination ul").append('<li><a href="#">' + pageNum + '</a></li>');
             pageNum += 1;
@@ -45,6 +49,8 @@ function paginateFunction(){
       //sets page 1 to the active page
      $(".pagination a").first().addClass("active");
 }
+
+
 
 //moves between each page showing the proper students upon each page click
 function pageClickFunction() {
@@ -56,31 +62,29 @@ function pageClickFunction() {
     var startSlice = (currentPage * 10) - 10;
     var endSlice = (currentPage * 10);
 
-    // if(currentPage === 1){
-    //     startSlice = currentPage - 1;
-    //     endSlice = currentPage + 9;
-    // }
     $(".student-item").hide();
     $(".student-item.cf").slice(startSlice,endSlice).show();
 }
 
-// searches students
+
+
+// searches students by getting input from the user and comparing each character to the text inside of "student-detials." Adds/removes students from "numberOfStudents" as need be.
 function searchFunction(){
 
     var $search = $(".student-search input").val().trim().toLowerCase().replace(/ /g,''); // gets user input from from search field, trims each end, changes to lowercaes, and removes spaces
-
     $(".student-details").each(function(){ // loops through each student
+    studentDetailsString = $(this).text().trim().toLowerCase().replace(/ /g,'');
 
-        studentDetailsString = $(this).text().trim().toLowerCase().replace(/ /g,'');
-
+        //if there are no matching characters for each input, the CF class is removed from each "student-item" class. Only students with the class CF are displayed
         if (studentDetailsString.search(new RegExp($search, "i")) < 0) {
                 $(this).closest(".student-item").removeClass("cf").hide();
             } else {
                 $(this).closest(".student-item").addClass("cf").show();
-              }
-        })
 
-  numberOfStudents = $(".student-item.cf").length;
+              }
+        });
+
+  numberOfStudents = $(".student-item.cf").length; //number of students with the CF class
 
   // checks to see if there are no search matches - dispalys "No students found!" if there are no results
   if(numberOfStudents === 0){
@@ -88,13 +92,12 @@ function searchFunction(){
   } else {
     $(".noStudentFound").hide();
   }
-
   // re-paginate with new search results after each keyup
   paginateFunction();
 }
 
 //---EVENT LISTENERS---//
 
-$(".pagination").on("click", "a", pageClickFunction);
-
-$(".student-search input").keyup(searchFunction);
+$(".pagination").on("click", "a", pageClickFunction); //switches between pages when each pagination link is clicked
+$('.page-header').find('button').on('click', searchFunction); //doesn't actually do anything because of the live search
+$(".student-search input").keyup(searchFunction); //calls "searchFunction" on each keyup
